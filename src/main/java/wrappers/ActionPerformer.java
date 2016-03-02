98277648d662b9ef8076e3e0971a7a7b6fda5a64
Asaf.Shochet <asaf.shochet@timetoknow.com>
2016-03-02 15:19:33 +0200

    added wikipedia presidents control

--- src/main/java/wrappers/ActionPerformer.java
+++ src/main/java/wrappers/ActionPerformer.java
@@ -2,6 +2,9 @@ package wrappers;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Asaf.Shochet on 2/29/2016.
@@ -11,13 +14,14 @@ public class ActionPerformer {
     public void perform(String action) throws IOException {
 
         TeacherAction teacherAction = TeacherAction.valueOf(action);
+        ChromeWrapper chromeWrapper;
         switch (teacherAction){
             case LOCK_DEVICE:
                 ComputerLocker computerLocker = new ComputerLocker();
                 computerLocker.lockComputer();
                 break;
             case OPEN_URL:
-                ChromeWrapper chromeWrapper = new ChromeWrapper();
+                chromeWrapper = new ChromeWrapper();
                 chromeWrapper.openAndNavigateToUrl("https://www.youtube.com/embed/YgHrn2VFbvo?autoplay=1");
                 break;
             case PLAY_AUDIO:
@@ -31,6 +35,13 @@ public class ActionPerformer {
                 String address = "asaf.shochet@timetoknow.com";
                 sendEmail.sendMessage(address);
                 break;
+            case OPEN_WIKIPEDIA:
+                chromeWrapper = new ChromeWrapper();
+                List<String> presidentsUrls = Arrays.asList("https://he.wikipedia.org/wiki/%D7%A9%D7%9E%D7%A2%D7%95%D7%9F_%D7%A4%D7%A8%D7%A1", "https://he.wikipedia.org/wiki/%D7%A2%D7%96%D7%A8_%D7%95%D7%99%D7%A6%D7%9E%D7%9F", "https://he.wikipedia.org/wiki/%D7%96%D7%9C%D7%9E%D7%9F_%D7%A9%D7%96%D7%A8","https://he.wikipedia.org/wiki/%D7%99%D7%A6%D7%97%D7%A7_%D7%91%D7%9F-%D7%A6%D7%91%D7%99", "https://he.wikipedia.org/wiki/%D7%97%D7%99%D7%99%D7%9D_%D7%95%D7%99%D7%A6%D7%9E%D7%9F");
+                String presidentUrl = presidentsUrls.get(new Random().nextInt(presidentsUrls.size()));
+                chromeWrapper.openAndNavigateToUrl(presidentUrl);
+                break;
+
         }
 
     }
